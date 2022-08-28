750fccda17d587137640c7cc9a63c2afa28cac34
Ekaterina Astaskina <ekaterina.fedoseeva@rt.ru>
2022-08-28 12:58:00 +0300

    added lesson 12

--- src/test/java/tests/simple/FailedTests.java
+++ src/test/java/tests/simple/FailedTests.java
@@ -1,9 +1,11 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@Tag("simple")
 public class FailedTests {
 
     @Test
--- src/test/java/tests/simple/PositiveTests.java
+++ src/test/java/tests/simple/PositiveTests.java
@@ -1,9 +1,11 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("simple")
 public class PositiveTests {
 
     @Test
